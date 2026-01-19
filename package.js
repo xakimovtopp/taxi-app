cd454b9a2a733a02b48d3909eb311080475aada2
xakimovtopp <xakimovtopp@gmail.com>
2026-01-19 18:08:33 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "taxi-pro-app",
+  "version": "1.0.0",
+  "description": "Taxi App",
+  "main": "server/index.js",
+  "scripts": {
+    "start": "node server/index.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "mongoose": "^8.0.0",
+    "socket.io": "^4.7.0"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
